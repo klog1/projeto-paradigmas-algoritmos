ea3328e1cffb00aa7e13fba5e05419ed9d8e3209
klog1 <kaueperezfernando@gmail.com>
2024-10-28 09:42:08 -0300

    update

--- main.py
+++ main.py
@@ -3,4 +3,6 @@
 # This is a simple Hello World program
 print("Hello, World!")
 
-# saber como abrir um arquiv.txt e abrir em uma lista
+# saber como abrir um arquiv.txt e abrir em uma lista oi
+
+print("daora")
